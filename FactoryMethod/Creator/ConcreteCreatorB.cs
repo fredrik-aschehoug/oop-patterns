32ffea94bf64875ec30e1091ae0fa37c29c0d772
Fredrik <50200428+fredrik-instech@users.noreply.github.com>
2021-11-21 16:08:22 +0100

    Add Factory Method Pattern

---
+++ FactoryMethod/Creator/ConcreteCreatorB.cs
@@ -0,0 +1,12 @@
+﻿using FactoryMethod.Product;
+
+namespace FactoryMethod.Creator
+{
+    class ConcreteCreatorB : Creator
+    {
+        public override IProduct CreateProduct()
+        {
+            return new ConcreteProductB();
+        }
+    }
+}
